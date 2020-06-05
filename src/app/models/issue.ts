903a4a43edcb2e5fa053502777160598956a4586
kosanamsaisudheer@gmail.com <Rajput@123>
2020-06-05 13:35:10 +0530

    first commit

---
+++ src/app/models/issue.ts
@@ -0,0 +1,8 @@
+export class Issue {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,56 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {Issue} from '../models/issue';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+
+
+
+@Injectable()
+export class DataService {
+ private readonly API_URL = '../../assets/data.json';
+
+  dataChange: BehaviorSubject<Issue[]> = new BehaviorSubject<Issue[]>([]);
+  // Temporarily stores data from dialogs
+  dialogData: any;
+
+  constructor (private httpClient: HttpClient) {}
+
+  get data(): Issue[] {
+    return this.dataChange.value;
+    console.log(this.dataChange);
+  }
+
+  getDialogData() {
+    return this.dialogData;
+  }
+
+  /** CRUD METHODS */
+  getAllIssues(): void {
+    this.httpClient.get<Issue[]>(this.API_URL).subscribe(data => {
+        console.log(data);
+        this.dataChange.next(data);
+      },
+      (error: HttpErrorResponse) => {
+      console.log (error.name + ' ' + error.message);
+      });
+  }
+
+  addIssue (issue: Issue): void {
+    this.dialogData = issue;
+  }
+
+  updateIssue (issue: Issue): void {
+    this.dialogData = issue;
+  }
+
+  deleteIssue (id: number): void {
+    console.log(id);
+  }
+}
+
+
+
+
+
+
+
